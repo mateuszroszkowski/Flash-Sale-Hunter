7c4ad9eef652986165a91c1a3c2ec96bcbfa5385
Mateusz Roszkowski <mroszko2@gmail.com>
2019-11-25 01:24:42 +0100

    Add support for a flash sale on x-kom.pl

--- hunter.py
+++ hunter.py
@@ -3,6 +3,8 @@ from tools import scrapers
 def main():
     scraper = scrapers.MoreleScraper()
     scraper.scrap()
+    scraper = scrapers.XKomScraper()
+    scraper.scrap()
 
 if __name__ == "__main__":
     main()
--- tools/scrapers.py
+++ tools/scrapers.py
@@ -58,6 +58,61 @@ class MoreleScraper(Scraper):
         sold = html.find("div", attrs={"class": "status-box-expired"}).text.replace('\n','')
         remaining = html.find("div", attrs={"class": "status-box-was"}).text.replace('\n','')
         sold, remaining = sold.split()[1], remaining.split()[1]
+        if remaining == "0":
+            sold, remaining = "ALL", "OUT OF STOCK"
         return sold, remaining
 
-    
+class XKomScraper(Scraper):
+
+    def scrap(self):
+        url = "https://www.x-kom.pl/"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            content = BeautifulSoup(response.content, "html5lib")
+            sale = content.find("div", attrs={"class": "col-md-4 col-sm-12"})
+            if sale is not None:
+                sale_name = sale.find("header", attrs={"class": "text-left col-xs-12 col-md-12 col-lg-7"}).text.replace('\n','').split()
+                sale_name = " ".join(word for word in sale_name)
+                item_name = sale.find("p", attrs={"class": "product-name"}).text.replace('\n','')
+                old_price, new_price = self._parse_prices_from_html(sale)
+                sold, remaining = self._parse_quantities_from_html(sale)
+                remaining_time = self._parse_end_time_from_html(sale)
+                sale = FlashSale(url[8:22], sale_name, item_name, old_price, new_price, sold, remaining, remaining_time)
+                print(sale)
+            else:
+                print("No flash sale found on {}".format(url))
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    def _parse_end_time_from_html(self, html):
+        script = html.findAll("script")[0]
+        end_time = self._select_time_object_from_script_body(script)
+        current_time = datetime.datetime.now().replace(microsecond=0)
+        end_time = end_time - current_time
+        hours, seconds = divmod(end_time.seconds, 3600)
+        minutes, seconds = divmod(seconds, 60)
+        return datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+
+    def _parse_prices_from_html(self, html):
+        old = html.find("div", attrs={"class": "old-price"}).text.replace('\n','')
+        new = html.find("div", attrs={"class": "new-price"}).text.replace('\n','')
+        old, new = old.split()[0], new.split()[0]
+        return old, new
+
+    def _parse_quantities_from_html(self, html):
+        try:
+            sold = html.find("div", attrs={"class": "pull-right"}).text.replace('\n','')
+            remaining = html.find("div", attrs={"class": "pull-left"}).text.replace('\n','')
+            sold, remaining = sold.split()[1], remaining.split()[1]
+        except AttributeError as e:
+            sold, remaining = "ALL", "OUT OF STOCK"
+        return sold, remaining
+
+    def _select_time_object_from_script_body(self, script):
+        time = re.findall(r"\bDate\(.+?\)", script.text)[0]
+        time = time[5:len(time) - 1]
+        time = [number for number in time.split(',') if number.isdigit()]
+        time = "{}-{}-{} {}:{}:{}".format(*time)
+        time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+        return time
